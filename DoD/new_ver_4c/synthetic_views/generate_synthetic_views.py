51996ea5a669c378ce3a67ba33fa3f6562e48484
youny626 <zhiru@uchicago.edu>
2022-07-11 10:34:44 -0500

    generate synthetic views

---
+++ DoD/new_ver_4c/synthetic_views/generate_synthetic_views.py
@@ -0,0 +1,73 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+def clear_dir(path):
+    for f in os.listdir(path):
+        os.remove(os.path.join(path, f))
+
+num_views = 1000
+num_rows = 1000
+
+key_col = np.arange(num_rows)
+other_col = np.zeros(num_rows, dtype=int)
+
+base_view = pd.DataFrame({"key": key_col, "other": other_col})
+# print(base_view)
+
+data_dir = "./new_ver_4c/synthetic_views"
+
+# Compatible
+# Generate n identical base views
+clear_dir(f"./synthetic_views/compatible/")
+for i in range(num_views):
+    base_view.to_csv(f"{data_dir}/{i}.csv", index=False)
+
+# Contained
+# For i in n:
+# Remove 1 random row in base view
+# Base view becomes the new view
+# Add the new view to the list
+clear_dir(f"{data_dir}/contained/")
+base_contained = base_view.copy()
+base_contained.to_csv(f"{data_dir}/contained/0.csv", index=False)
+for i in range(1, num_views):
+    # num_rows_to_remove = np.random.randint(1, len(base_contained)+1)
+    drop_indices = np.random.choice(base_contained.index)
+    base_contained = base_contained.drop(drop_indices)
+    base_contained.to_csv(f"{data_dir}/contained/{i}.csv", index=False)
+
+
+# Complementary
+# Max = n
+# For i in n:
+# Change 1 of random key value in base view to some other value (while still maintaining uniqueness and not causing contradiction)
+# random number = m
+# For j in range(m):
+# Change the key value to max + j
+# Max = n + m
+# Add the new view to the list
+clear_dir("{data_dir}/complementary/")
+max_key_value = num_views - 1
+for i in range(num_views):
+    random_index = np.random.choice(base_view.index)
+    new_view = base_view.copy()
+    new_view.iloc[random_index]["key"] = max_key_value + 1
+    max_key_value += 1
+    new_view.to_csv(f"{data_dir}/complementary/{i}.csv", index=False)
+
+
+# Contradictory
+# For i in n:
+# Choose 1 random row in base view and change the non-key column value to some other value
+# Add the new view to the list
+clear_dir("{data_dir}/contradictory/")
+max_other_value = 0
+for i in range(num_views):
+    random_index = np.random.choice(base_view.index)
+    new_view = base_view.copy()
+    new_view.iloc[random_index]["other"] = max_other_value + 1
+    max_other_value += 1
+    new_view.to_csv(f"{data_dir}/contradictory/{i}.csv", index=False)
+
