720caef6e43e9be518f4cd2b674599df57becc1a
Sainyam Galhotra <sainyamgalhotra@gmail.com>
2022-11-09 18:24:39 -0800

    added dataset interface with content similarity

---
+++ view_presentation/interface/dataset_interface_content_sim.py
@@ -0,0 +1,79 @@
+import pandas as pd
+from interface import interface
+import embedding_distance
+
+class DatasetInterfaceContentSim(interface):
+    def __init__(self,name):
+        self.name=name
+        self.asked_questions={}
+        self.curr_question_iter=0
+
+    def generate_candidates (self, df_lst):
+        self.content_dic={}
+        iter=0
+        while iter<len(df_lst):
+            df=df_lst[iter]
+            try:
+                attr_lst= list(df.columns)
+                df_content =''
+                for attr in attr_lst:
+                    df_content+=' '.join(list(df[attr].values))+' '
+
+                self.content_dic[iter] = df_content
+                iter+=1
+            except:
+                iter+=1
+                continue
+
+    def rank_candidates (self, query, embedding_obj): 
+        self.scores={}
+        for df_iter in self.content_dic.keys():
+            dist=embedding_obj.get_distance(self.content_dic[df_iter],query)#model.wmdistance(attr.split(),query.split())
+            self.scores[df_iter]=dist
+        self.sorted_sc=sorted(self.scores.items(), key=lambda item: item[1],reverse=False)
+        return self.sorted_sc
+
+    #Returns the data frame with the highest score
+    def get_question(self, ignore_questions=[]):
+        iter=self.curr_question_iter
+        while iter<len(self.sorted_sc):
+            if self.sorted_sc[iter][0] in ignore_questions:
+                iter+=1
+                continue
+            else:
+                break
+        curr_question =self.sorted_sc[iter][0]
+        self.curr_question_iter = iter
+        #returns the location of chosen df 
+        return (iter, curr_question)
+
+    #TODO: Change the value of dictionary to the answer from the user
+    def ask_question(self, iter, question):
+        self.curr_question_iter=iter+1
+        self.asked_questions[question] = 1
+
+
+#example usage of the interface
+if __name__ == '__main__':
+
+    data1 = ["Chicago","NYC","SF","Seattle"]
+    df1 = pd.DataFrame(data1, columns=['City'])
+  
+    data2 = ["Paris","Copenhagen","Delhi","Sydney"]
+    df2 = pd.DataFrame(data2, columns=['international city'])
+
+    df_lst=[df1,df2]
+
+   
+    print ("Dataset Interface")
+    attr_inf=DatasetInterfaceContentSim("header content interface")
+    attr_inf.generate_candidates(df_lst)
+    print(attr_inf.content_dic)
+
+    embedding_obj = embedding_distance.EmbeddingModel()
+    print(attr_inf.rank_candidates("new york city",embedding_obj))
+
+
+    print(attr_inf.get_question())
+
+    
