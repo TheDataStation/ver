1e0e6c57c4878af287f46d3fb518d90fe2c59735
Sainyam Galhotra <sainyamgalhotra@gmail.com>
2023-03-07 12:53:24 -0800

    view ranking

--- view_presentation/ver_view_presentation.py
+++ view_presentation/ver_view_presentation.py
@@ -6,10 +6,12 @@ import view_presentation.interface.embedding_distance as embedding_distance
 import view_presentation.config as config
 import view_presentation.interface_list as interface_lst
 import view_presentation.interface as int_folder
+import ipywidgets as widgets
+
+from IPython.display import clear_output
 
 random.seed(config.seed)
 
-#Todo implement a function to use shortlisted and ignored datasets and rank the options
 #Right now, same question can be asked by multiple interfaces. Ignored candidates of different interfaces do not share information.
 #Sharing that info can improve complexity
 
@@ -37,15 +39,54 @@ class ViewPresentation:
         iter=0
         self.df_lst=df_lst
         for curr_interface in interface_lst.interface_options:
-            aci = curr_interface(str(iter))
+            aci = curr_interface(str(iter),self.embedding_obj)
             aci.generate_candidates(self.df_lst)
-            aci.rank_candidates(self.query,self.embedding_obj)
+            aci.rank_candidates(self.query)
             self.interface_options.append(aci)
             iter+=1
     
         
 
     # ranking of questions
+    def update_output(self,b):
+        #print ("updated output")
+        res= (self.result)
+        coverage_lst=res[-1]
+        self.choose_interface()
+        out_index= res[0].index(res[1].value)
+        
+        if out_index==0:
+            for df_iter in coverage_lst:
+                if df_iter in self.shortlisted_datasets.keys():
+                    self.shortlisted_datasets[df_iter]+=1
+                else:
+                    self.shortlisted_datasets[df_iter]=1
+        elif out_index==1:
+            for df_iter in coverage_lst:
+                if df_iter in self.ignored_datasets.keys():
+                    self.ignored_datasets[df_iter]+=1
+                else:
+                    self.ignored_datasets[df_iter]=1
+
+
+        print ("this",out_index,self.shortlisted_datasets,self.ignored_datasets)
+
+        return
+    def get_shortlisted_datasets(self):
+        print ("shortlisted datasets are")
+        '''for iter in shortlisted:
+            print ("---------------------")
+            display(self.df_lst[iter])
+            print ("---------------------")
+        '''
+        final_scores={}
+        for df_iter in self.shortlisted_datasets.keys():
+            final_scores[df_iter] = self.shortlisted_datasets[df_iter]
+            if df_iter in self.ignored_datasets.keys():
+                final_scores[df_iter]-=self.ignored_datasets[df_iter]
+        scores = sorted(final_scores.items(), key=lambda item: item[1],reverse=True)
+        print (scores)
+
 
     def choose_interface(self):
 
@@ -62,9 +103,11 @@ class ViewPresentation:
         coverage_lst=[]
 
         for interface in self.interface_options:#move to config.py
-            
-            score,ques,coverage=interface.get_question(list(self.ignored_datasets.keys()))
-            print (interface.name,score,ques,coverage)
+            try:
+                score,ques,coverage=interface.get_question(list(self.ignored_datasets.keys()))
+            except:
+                continue
+            #print (interface.name,score,ques,coverage)
             answer_prob=1
             if ques is not None and  self.asked[interface]<threshold:
                 choose_random=True
@@ -82,6 +125,7 @@ class ViewPresentation:
             return None,None,None
         if choose_random:
             max_index=random.randint(0,len(scores)-1)#scores.index(max(scores))
+            #print (max_index,"choosing randomly")
         else:
             #Toss a coin with prob. TODO
             total_score=sum(scores)
@@ -89,7 +133,7 @@ class ViewPresentation:
             randval=random.random()
 
             for interface in valid_interfaces:
-                print (i)
+                #print (i)
                 if total_score>0:
                     scores[i]=(1-gamma)*scores[i]*1.0/total_score + gamma*1.0/len(valid_interfaces)
                 else:
@@ -99,9 +143,12 @@ class ViewPresentation:
                 randval-=scores[i]
                 i+=1
             max_index=i
-        print ("Current question",max_index)
+        #print ("Current question",max_index)
 
-        result = valid_interfaces[max_index].ask_question(corresponding_ques[max_index],self.df_lst)
+        self.result = valid_interfaces[max_index].ask_question(corresponding_ques[max_index],self.df_lst)
+        self.result.append(coverage_lst[max_index])
+        self.result[2].on_click(self.update_output)
+        '''
         if result==1:
             for df_iter in coverage_lst[max_index]:
                 if df_iter in self.shortlisted_datasets.keys():
@@ -116,7 +163,8 @@ class ViewPresentation:
                     self.ignored_datasets[df_iter]=1
 
 
-        print (self.shortlisted_datasets,self.ignored_datasets)
+        print ("this",self.shortlisted_datasets,self.ignored_datasets)
+        '''
         #Use responses to update shortlisted and ignored ones
 
 
