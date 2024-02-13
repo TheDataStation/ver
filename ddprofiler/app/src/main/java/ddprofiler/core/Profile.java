36263cefe84eb2ca5d6559660ba24f12cf33ad48
David Alexander <david.alexander01@ui.ac.id>
2024-02-13 11:49:40 +0700

    refactor Profile creation

--- ddprofiler/app/src/main/java/ddprofiler/core/Profile.java
+++ ddprofiler/app/src/main/java/ddprofiler/core/Profile.java
@@ -1,23 +1,53 @@
 package ddprofiler.core;
 
-public record Profile (long id,
-                       String dbName,
-                       String path,
-                       String sourceName,
-                       String columnName,
-                       String dataType,
-                       String columnLabel,
-                       int totalValues,
-                       int uniqueValues,
-                       int nonEmptyValues,
-                       String entities,
-                       long[] minhash,
-                       String xstructure,
-                       float minValue,
-                       float maxValue,
-                       float avgValue,
-                       long median,
-                       long iqr) {
+public record Profile(
+        long id,
+        String dbName,
+        String path,
+        String sourceName,
+        String columnName,
+        String dataType,
+        String columnLabel,
+        int totalValues,
+        int uniqueValues,
+        int nonEmptyValues,
+        String entities,
+        long[] minhash,
+        String xstructure,
+        float minValue,
+        float maxValue,
+        float avgValue,
+        long median,
+        long iqr,
+        String spatialTemporalType,
+        String granularity
+) {
+    public Profile(
+            long id,
+            String dbName,
+            String path,
+            String sourceName,
+            String columnName,
+            String dataType,
+            String columnLabel,
+            int totalValues,
+            int uniqueValues,
+            int nonEmptyValues,
+            String entities,
+            long[] minhash,
+            String xstructure,
+            float minValue,
+            float maxValue,
+            float avgValue,
+            long median,
+            long iqr
+    ) {
+
+        // Made for backwards compatibility of when spatialTemporalType and granularity were not part of the Profile
+        this(id, dbName, path, sourceName, columnName, dataType, columnLabel, totalValues, uniqueValues, nonEmptyValues,
+                entities, minhash, xstructure, minValue, maxValue, avgValue, median, iqr,
+                null, null);
+    }
 
     public float minValue() {
         if (Double.isNaN(minValue) || Double.isInfinite(minValue)) {
